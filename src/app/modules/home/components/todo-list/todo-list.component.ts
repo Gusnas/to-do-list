9a19f7c35007817ef2ff22f8e98676f562ef4ec9
Gustavo Nascimento <gusnas@alu.ufc.br>
2023-02-15 21:59:20 -0300

    styles + delete and list logic

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TaskList } from '../../model/task-list';
 
 @Component({
   selector: 'app-todo-list',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoListComponent implements OnInit {
 
+  public taskList: Array<TaskList> = [
+    {task: "Minha nova Task", checked: true},
+    {task: "Minha nova Task 2", checked: false}
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
   }
+  
+  public deleteItemTaskList(event: number){
+    this.taskList.splice(event, 1);
+  }
 
+  public deleteAllTaskList(){
+    const confirm = window.confirm("Você deseja realmente deletar tudo?")
+    if(confirm){
+      this.taskList = [];
+    }    
+  }
 }
